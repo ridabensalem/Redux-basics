452fc54deac703d2e60c64225440a80539ac71a1
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-10 12:55:23 +0100

    Intialize the react tutorial

---
+++ basics/src/redux /bookslice.jsx
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+cartItems: [],
+amount: 0,
+total: 0,
+isLoading: true,
+};
+const cartSlice = createSlice({
+name: 'cart',
+initialState,
+});
+export default cartSlice;
